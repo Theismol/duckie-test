e33a2951d7f5b8e529ea7bd16fb10cfca59d8f3e
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-11-21 10:38:32 +0100

    unit test lavet til A start , ligger i test mappen

--- packages/my_package/src/test/A_star_unit_test.py
+++ packages/my_package/src/test/A_star_unit_test.py
@@ -1,5 +1,4 @@
-import heapq
-
+import unittest
 
 
 def astar(start, goal, graph):
@@ -53,48 +52,10 @@ def heuristic(node, goal):
     return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
 
 
-def get_directions(path):
-    directions = []
-    directions_numbers = []
-    for i in range(len(path) - 1):
-        current_node = path[i]
-        next_node = path[i + 1]
-        if next_node[0] > current_node[0]:
-            directions.append('down')
-        elif next_node[0] < current_node[0]:
-            directions.append('up')
-        elif next_node[1] > current_node[1]:
-            directions.append('right')
-        elif next_node[1] < current_node[1]:
-            directions.append('left')
-    current_direction = directions[0]
-    for direction in directions:
-        if direction == current_direction:
-            directions_numbers.append(1)
-        elif direction == "up" and current_direction == "right" or direction == "right" and current_direction == "down" or direction == "down" and current_direction == "left" or direction == "left" and current_direction == "up":
-            directions_numbers.append(2)
-            directions_numbers.append(1)
-            current_direction=direction
-        elif direction == "up" and current_direction == "left" or direction == "left" and current_direction == "down" or direction == "down" and current_direction == "right" or direction == "right" and current_direction == "up":
-            directions_numbers.append(0)
-            directions_numbers.append(2)
-            current_direction=direction
-    return directions_numbers
 
-
-
-
-
-def calculate_cost_of_path(path, graph):
-    cost = 0
-    for i in range(len(path) - 1):
-        cost += graph[path[i]][path[i + 1]]
-    return cost
-
-
-
-
-graph = {
+class TestAStar(unittest.TestCase):
+    def setUp(self):
+        self.graph= {
     (0, 0): {},
     (0, 1): {},
     (0, 2): {},
@@ -288,20 +249,224 @@ graph = {
     (11, 14): {(11,15):1},
     (11, 15): {}
 }
+        self.start_node = (7, 0)
+        self.goal_node = (0, 9)
 
+    def test_astar_found_path(self):
+        path, cost = astar(self.start_node, self.goal_node, self.graph)
+        self.assertIsNotNone(path)
+        self.assertIsNotNone(cost)
+        self.assertIsInstance(path, list)
+        self.assertIsInstance(cost, int)
+        self.assertEqual(path[0], self.start_node)
+        self.assertEqual(path[-1], self.goal_node)
+        self.assertTrue(cost > 0)  # Assuming the cost is positive for a valid path
 
-# Example start and goal nodes
+    def test_astar_no_path(self):
+        # Modify graph to make it impossible to reach the goal from the start
+        # For instance, remove connections or add obstacles that block the path
+        # Then test if the function returns None for both path and cost
+        modified_graph = {
+    (0, 0): {},
+    (0, 1): {},
+    (0, 2): {},
+    (0, 3): {},
+    (0, 4): {(1,4):1},
+    (0, 5): {(0,4):1},
+    (0, 6): {(0,5):1},
+    (0, 7): {(0,6):1},
+    (0, 8): {(0,7):1},
+    (0, 9): {(0,8):1},
+    (0, 10): {(0,9):1},
+    (0, 11): {(0,10):1},
+    (0, 12): {},
+    (0, 13): {},
+    (0, 14): {},
+    (0, 15): {},
+    (1, 0): {},
+    (1, 1): {},
+    (1, 2): {},
+    (1, 3): {},
+    (1, 4): {(2,4):1},
+    (1, 5): {(1,6):1},
+    (1, 6): {(1,7):1},
+    (1, 7): {(1,8):1},
+    (1, 8): {(1,9):1},
+    (1, 9): {(1,10):1},
+    (1, 10): {(2,10):1},
+    (1, 11): {(0,11):1},
+    (1, 12): {},
+    (1, 13): {},
+    (1, 14): {},
+    (1, 15): {},
+    (2, 0): {},
+    (2, 1): {},
+    (2, 2): {},
+    (2, 3): {},
+    (2, 4): {(3,4):1},
+    (2, 5): {(1,5):1},
+    (2,6): {},
+    (2, 7): {},
+    (2, 8): {},
+    (2, 9): {},
+    (2, 10): {(3,10):1},
+    (2, 11): {(1,11):1},
+    (2, 12): {},
+    (2, 13): {},
+    (2, 14): {},
+    (2, 15): {},
+    (3, 0): {},
+    (3, 1): {},
+    (3, 2): {},
+    (3, 3): {},
+    (3, 4): {(4,4):1},
+    (3, 5): {(2,5):1},
+    (3, 6): {},
+    (3, 7): {},
+    (3, 8): {},
+    (3, 9): {},
+    (3, 10): {(4,10):1},
+    (3, 11): {(2,11):1},
+    (3, 12): {},
+    (3, 13): {},
+    (3, 14): {},
+    (3, 15): {},
+    (4, 0): {},
+    (4, 1): {},
+    (4, 2): {(5,2):1},
+    (4, 3): {(4,2):1},
+    (4, 4): {(4,3):1,(5,4):20},
+    (4, 5): {(4,4):20,(3,5):1},
+    (4, 6): {},
+    (4, 7): {},
+    (4, 8): {(5,8):1},
+    (4, 9): {(4,8):1},
+    (4, 10): {(4,9):1,(5,10):20},
+    (4, 11): {(4,10):20,(3,11):1},
+    (4, 12): {(4,11):1},
+    (4, 13): {(4,12):1},
+    (4, 14): {},
+    (4, 15): {},
+    (5, 0): {},
+    (5, 1): {},
+    (5, 2): {(6,2):1},
+    (5, 3): {(5,4):1},
+    (5, 4): {(6,4):1,(5,5):20},
+    (5, 5): {(4,5):20},
+    (5, 6): {},
+    (5, 7): {},
+    (5, 8): {(6,8):1},
+    (5, 9): {(5,10):1},
+    (5, 10): {(5,11):20},
+    (5, 11): {(4,11):20,(5,12):1},
+    (5, 12): {(6,12):1},
+    (5, 13): {(4,13):1},
+    (5, 14): {},
+    (5, 15): {},
+    (6, 0): {},
+    (6, 1): {(6,0):1},
+    (6, 2): {(6,1):1,(7,2):20},
+    (6, 3): {(6,2):20,(5,3):1},
+    (6, 4): {(7,4):1},
+    (6, 5): {(5,5):1},
+    (6, 6): {},
+    (6, 7): {},
+    (6, 8): {(7,8):1},
+    (6, 9): {(5,9):1},
+    (6, 10): {},
+    (6, 11): {},
+    (6, 12): {(7,12):1},
+    (6, 13): {(5,13):1},
+    (6, 14): {},
+    (6, 15): {},
+    (7, 0): {},
+    (7, 1): {(7,2):1},
+    (7, 2): {(7,3):20,(8,2):1},
+    (7, 3): {(6,3):20},
+    (7, 4): {(8,4):1},
+    (7, 5): {(6,5):1},
+    (7, 6): {},
+    (7, 7): {},
+    (7, 8): {(8,8):1},
+    (7, 9): {(6,9):1},
+    (7, 10): {},
+    (7, 11): {},
+    (7, 12): {(8,12):1},
+    (7, 13): {(6,13):1},
+    (7, 14): {},
+    (7, 15): {},
+    (8, 0): {},
+    (8, 1): {},
+    (8, 2): {(9,2):1},
+    (8, 3): {(7,3):1},
+    (8, 4): {(9,4):1},
+    (8, 5): {(7,5):1},
+    (8, 6): {},
+    (8, 7): {},
+    (8, 8): {(9,8):1},
+    (8, 9): {(7,9):1},
+    (8, 10): {},
+    (8, 11): {},
+    (8, 12): {(9,12):1},
+    (8, 13): {(7,13):1},
+    (8, 14): {},
+    (8, 15): {},
+    (9, 0): {},
+    (9, 1): {},
+    (9, 2): {(10,2):1},
+    (9, 3): {(8,3):1},
+    (9, 4): {(10,4):1},
+    (9, 5): {(8,5):1},
+    (9, 6): {},
+    (9, 7): {},
+    (9, 8): {(10,8):1},
+    (9, 9): {(8,9):1},
+    (9, 10): {},
+    (9, 11): {},
+    (9, 12): {(10,12):1},
+    (9, 13): {(8,13):1},
+    (9, 14): {},
+    (9, 15): {},
+    (10, 0): {},
+    (10, 1): {},
+    (10, 2): {(11,2):1},
+    (10, 3): {(9,3):1},
+    (10, 4): {(10,3):1,(11,4):20},
+    (10, 5): {(9,5):1,(10,4):20},
+    (10, 6): {(10,5):1},
+    (10, 7): {(10,6):1},
+    (10, 8): {(10,7):1,(11,8):20},
+    (10, 9): {(9,9):1,(10,8):20},
+    (10, 10): {(10,9):1},
+    (10, 11): {(10,10):1},
+    (10, 12): {(10,11):1,(11,12):20},
+    (10, 13): {(9,13):1,(10,12):20},
+    (10, 14): {(10,13):1},
+    (10, 15): {(10,14):1},
+    (11, 0): {},
+    (11, 1): {},
+    (11, 2): {(11,3):1},
+    (11, 3): {(11,4):1},
+    (11, 4): {(11,5):20},
+    (11, 5): {(11,6):1,(10,5):20},
+    (11, 6): {(11,7):1},
+    (11, 7): {(11,8):1},
+    (11, 8): {(11,9):20},
+    (11, 9): {(11,10):1,(10,9):20},
+    (11, 10): {(11,11):1},
+    (11, 11): {(11,12):1},
+    (11, 12): {(11,13):20},
+    (11, 13): {(11,14):1,(10,13):20},
+    (11, 14): {(11,15):1},
+    (11, 15): {}
+}
+        # Make modifications to the graph
 
-if __name__ == "__main__":
-    start_node = (7, 0)
-    goal_node = (0, 9)
+        path, cost = astar(self.start_node, self.goal_node, modified_graph)
+        self.assertIsNone(path)
+        self.assertIsNone(cost)
 
-    path, cost = astar(start_node, goal_node, graph)
-    if path:
-        print("Path found:", path)
-        print("Total cost:", cost)
-    else:
-        print("No path found")
+    # Add more test cases if needed
 
-
-# Create an image to visualize the maze and the path
+if __name__ == '__main__':
+    unittest.main()
