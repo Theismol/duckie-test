07aea32b4f2dbfe7d049a04b0ab006a3aba173e1
alexandermander <alexandermander123@gmail.com>
2023-12-07 18:44:09 +0100

    updede

--- packages/my_package/src2/ld_node.py
+++ packages/my_package/src2/ld_node.py
@@ -19,44 +19,39 @@ class edgeFeature:
         self.color = color
 
 
-class CameraReaderNode(DTROS):
-    #def __init__(self, node_name):
-       # self.right = False
-       # self.left = False      # initialize the DTROS parent class
-       # self.no_change = False
-       # kmeans = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model1.sav', 'rb'))
-       # kmeans1 = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model.sav', 'rb'))
-       # self.centroids = kmeans.cluster_centers_
-       # self.centroids1 = kmeans1.cluster_centers_
-       # super(CameraReaderNode, self).__init__(node_name=node_name, node_type=NodeType.VISUALIZATION)
-       # # static parameters
-       # self._vehicle_name = os.environ['VEHICLE_NAME']
-       # self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
-       # # bridge between OpenCV and ROS
-       # self._bridge = CvBridge()
-       # # create window
-       # self._window = "camera-reader"
-       # cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
-       # self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
-       # self.pub = rospy.Publisher("lane_detection_correction", String, queue_size=1)
-    def __init__(self, path):
-        kmeans = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model3.sav', 'rb'))
+class CameraReaderNode:
+    def __init__(self, node_name):
+        self.right = False
+        self.left = False      # initialize the DTROS parent class
+        self.no_change = False
+        kmeans = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model1.sav', 'rb'))
         kmeans1 = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model.sav', 'rb'))
         self.centroids = kmeans.cluster_centers_
         self.centroids1 = kmeans1.cluster_centers_
-        images = [f for f in os.listdir(path) if f.startswith('img')]
-        return images
+        super(CameraReaderNode, self).__init__(node_name=node_name, node_type=NodeType.VISUALIZATION)
+        # static parameters
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
+        # bridge between OpenCV and ROS
+        self._bridge = CvBridge()
+        # create window
+        self._window = "camera-reader"
+        cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
+        self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
+        self.pub = rospy.Publisher("lane_detection_correction", String, queue_size=1)
 
     def callback(self, msg):
         # convert JPEG bytes to CV image
         image, edges = self._bridge.compressed_imgmsg_to_cv2(msg)
-        #findFertures(image)
-        self.check_coordinates(image, edges)
+        self.findFertures(image)
+        #check_coordinates(image, edges)
         #Display the image with the detected lines and midpoint
         cv2.imshow(self._window, image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()  # Close OpenCV windows
             rospy.signal_shutdown("User pressed 'q' key")
+
+
 
     def findFertures(self, img):
         #create a list of egdepoligons for each image
@@ -94,32 +89,41 @@ class CameraReaderNode(DTROS):
             cv2.drawContours(img, contours, i, color, 2)
         return img, edgesFeatures
     
-    def closest_centroid(self, point):
-        first_centroid = self.centroids
-        second_centroid = self.centroids2
-        currnt_shortest_distance = 100000
-        someString = (255, 0, 0)
-        for i in range(len(first_centroid)):
-            #find the distance between the point and the first centroid
-            distance1 = sqrt((point[0][0] - first_centroid[i][0])**2 + (point[0][1] - first_centroid[i][1])**2 + (point[0][2] - first_centroid[i][2])**2)
-            #find the distance between the point and the second centroid
-            distance2 = sqrt((point[0][0] - second_centroid[i][0])**2 + (point[0][1] - second_centroid[i][1])**2 + (point[0][2] - second_centroid[i][2])**2)
-            if distance1 < currnt_shortest_distance:
-                #white stripe
-                someString = (0, 0,255)
-                currnt_shortest_distance = distance1
-            if distance2 < currnt_shortest_distance:
-                #yellow stripe
-                someString = (255, 0,0)
-                currnt_shortest_distance = distance2
-        return someString
     
     
-if __name__ == "__main__":
-    node = CameraReaderNode("/imgs")
-    imges = node.findFertures()
-    for i in range(len(imges)):
-        node.findFertures()
+kmeans = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model2.sav', 'rb'))
+kmeans1 = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model.sav', 'rb'))
+centroids = kmeans.cluster_centers_
+centroids2 = kmeans1.cluster_centers_
 
-    # keep spinning
-    rospy.spin()
+def closest_centroid(point):
+    first_centroid = centroids
+    second_centroid = centroids2
+    currnt_shortest_distance = 100000
+    someString = (255, 0, 0)
+    for i in range(len(first_centroid)):
+        #find the distance between the point and the first centroid
+        distance1 = sqrt((point[0][0] - first_centroid[i][0])**2 + (point[0][1] - first_centroid[i][1])**2 + (point[0][2] - first_centroid[i][2])**2)
+        #find the distance between the point and the second centroid
+        distance2 = sqrt((point[0][0] - second_centroid[i][0])**2 + (point[0][1] - second_centroid[i][1])**2 + (point[0][2] - second_centroid[i][2])**2)
+        if distance1 < currnt_shortest_distance:
+            #white stripe
+            someString = (0, 0,255)
+            currnt_shortest_distance = distance1
+        if distance2 < currnt_shortest_distance:
+            #yellow stripe
+            someString = (255, 0,0)
+            currnt_shortest_distance = distance2
+    return someString
+
+#if __name__ == "__main__":
+#    node = CameraReaderNode()
+#    for i in range(len(node.images)):
+#        img = cv2.imread(node.images[i])
+#        img = cv2.imread("C:\\Users\\alexa\\sanderStuff\\duckie-test\\packages\\my_package\\src2\\imgs\\" + node.images[i])
+#        img, edgesFeatures = node.findFertures(img)
+#        cv2.imshow("img", img)
+#        cv2.waitKey(0)
+#        cv2.destroyAllWindows()
+#
+
