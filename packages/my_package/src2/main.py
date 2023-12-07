6f28314ef9a901d4ea48aa6e69f31a97cdf6607a
alexandermander <alexandermander123@gmail.com>
2023-12-07 19:29:38 +0100

    add stuf

--- packages/my_package/src2/main.py
+++ packages/my_package/src2/main.py
@@ -1,44 +1,36 @@
-#import threading
-#import time
-#from queue import Queue
-#
-#import a_star as a_star
-#import lf_node as lf_node
-#
-#directions = []
-#lane_queue = Queue()
-#
-#
-#def generate_directions():
-#    global path
-#    start_node = (7, 0)
-#    goal_node = (0, 9)
-#    path = a_star.astar(start_node, goal_node, a_star.graph)[0]
-#    return a_star.get_directions(path)
-#
-#
-#def lane_following_monitor():
-#    while True:
-#        if not lane_queue.empty():
-#            directions = lane_queue.get()
-#            print(directions)
-#        time.sleep(0.1)
-#
-#
-#
-#if __name__ == '__main__':
-#    start_node = (10, 6)
-#    goal_node = (10, 2)
-#    
-#    directions = generate_directions()
-#    
-#    print(directions)
-#    
-#   
+import threading
+import time
+from queue import Queue
 
-import ld_node as ld_node
+import a_star as a_star
+import lf_node as lf_node
 
-node = ld_node.CameraReaderNode()
+directions = []
+lane_queue = Queue()
+
+
+def generate_directions():
+    global path
+    start_node = (7, 0)
+    goal_node = (0, 9)
+    path = a_star.astar(start_node, goal_node, a_star.graph)[0]
+    return a_star.get_directions(path)
+
+
+def lane_following_monitor():
+    while True:
+        if not lane_queue.empty():
+            directions = lane_queue.get()
+            print(directions)
+        time.sleep(0.1)
 
 
 
+if __name__ == '__main__':
+    start_node = (10, 6)
+    goal_node = (10, 2)
+    
+    directions = generate_directions()
+    
+    print(directions)
+    
