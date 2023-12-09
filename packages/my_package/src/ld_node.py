cf9278cf35bc3df8f43a81873423d167ba1bdf28
alexandermander <alexandermander123@gmail.com>
2023-12-09 11:54:35 +0100

    new lf

--- packages/my_package/src/ld_node.py
+++ packages/my_package/src/ld_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import lineCalculation as lc
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
@@ -13,11 +13,18 @@ from math import sqrt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-class edgeFeature:
-    def __init__(self, x, y, color ):
+class edgePoligon:
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
+        self.nameColor = self.findColor()
+
+    def findColor(self):
+        if self.color == (0, 0, 255):
+            return "red"
+        elif self.color == (255, 0, 0):
+            return "blue"
 
 
 class CameraReaderNode(DTROS):
@@ -34,67 +41,11 @@ class CameraReaderNode(DTROS):
         self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
         self.pub = rospy.Publisher("lane_detection_correction", String, queue_size=1)
 
-    def check_coordinates(self,image,edges):
-        y_yellowstripes = 0
-        x_yellowstripes = 0
-
-        x_whiteline = 0
-        y_whiteline = 0
-
-        average_yellowstripes = 0
-        average_whiteline = 0
-        #divides edges into yellow and white
-        for edge in edges:
-            if edge.color[0] == 255:
-                x_yellowstripes += edge.x
-                y_yellowstripes += edge.y
-                average_yellowstripes+= 1
-            else:
-                if edge.x < 150:
-                    continue
-                x_whiteline += edge.x
-                y_whiteline += edge.y
-                average_whiteline += 1
-        if average_whiteline == 0:
-            x_whiteline = 0
-            y_whiteline = 0
-        else:
-            x_whiteline = x_whiteline/average_whiteline
-            y_whiteline = y_whiteline/average_whiteline
-        if average_yellowstripes == 0:
-            x_yellowstripes = 0
-            y_yellowstripes = 0
-        else:
-            x_yellowstripes = x_yellowstripes/average_yellowstripes
-            y_yellowstripes = y_yellowstripes/average_yellowstripes
-        _, current_middle, _ = image.shape
-        current_middle = current_middle/2
-        if (x_whiteline - current_middle) > 230:
-            print("turn right")
-            self.right = True
-            self.left = False
-            self.no_change = False
-            message = "right"
-        elif current_middle - x_yellowstripes > 250:
-            print("turn left")
-            self.left = True
-            self.right = False
-            self.no_change = False
-            message = "left"
-        else:
-            print("no change in direction needed")
-            self.no_change = True
-            self.right = False
-            self.left = False
-            message = "no_change"
-
-        self.pub.publish(message)
 
     def callback(self, msg):
         # convert JPEG bytes to CV image
         image = self._bridge.compressed_imgmsg_to_cv2(msg)
         image, edges = self.findFertures(image)
-        self.check_coordinates(image, edges)
         #check_coordinates(image, edges)
         #Display the image with the detected lines and midpoint
         cv2.imshow(self._window, image)
@@ -103,8 +54,9 @@ class CameraReaderNode(DTROS):
             rospy.signal_shutdown("User pressed 'q' key")
 
 
-
     def findFertures(self, img):
+        egdepoligonRed = []
+        egdepoligonBlue = []
         #create a list of egdepoligons for each image
         height, width, _ = img.shape
         mask = np.zeros_like(img)
@@ -118,27 +70,32 @@ class CameraReaderNode(DTROS):
         #show the hlaf high in imshow
         edges = cv2.Canny(gray_roi, 100, 200)
         contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        edgesFeatures = []
         for i in range(len(contours)):
             area = cv2.contourArea(contours[i])
-            if area < 80:
+            if area < 50:
                 continue
             M = cv2.moments(contours[i])
             x = int(M["m10"] / M["m00"])
             y = int(M["m01"] / M["m00"])
-            #add the high of the mask to the y value
-            y = y + (height // 2)
+            y = y + height // 2
             b, g, r = img[y, x]
             #r g b as np array
             currebtPoint = np.array([[r, g, b]])
             #find the color of the point
             color = closest_centroid(currebtPoint)
-            edgesFeatures.append(edgeFeature(x,y,color))
-            #add the y offset to the i element in the contour list
-            for j in range(len(contours[i])):
-                contours[i][j][0][1] = contours[i][j][0][1] + (height // 2)
-            cv2.drawContours(img, contours, i, color, 2)
-        return img, edgesFeatures
+            #rgb
+            if color == (0, 0, 255):
+                egdepoligonRed.append(edgePoligon(x, y, color))
+            elif color == (255, 0, 0):
+                egdepoligonBlue.append(edgePoligon(x, y, color))
+            #draw the circle
+            cv2.circle(img, (x, y), 5, (0, 255, 0), -1)
+            #show   
+            x1, y1, x2, y2 = lc.mainRed(egdepoligonRed, width)
+            mx1, my1, mx2, my2 = lc.mainBlue(egdepoligonBlue, width)
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.line(img, (mx1, my1), (mx2, my2), (0, 0, 255), 3)
+        return img
     
 kmeans = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model2.sav', 'rb'))
 kmeans1 = pickle.load(open('/code/catkin_ws/src/<duckie-test>/packages/my_package/src/imgs/finalized_model.sav', 'rb'))
---
+++ packages/my_package/src/lineCalculation.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def mainRead(edgepoligonRed, width):
+    m, b = fit_line_through_points(egdepoligonRed)
+    x1 = 0
+    y1 = int(m * x1 + b)
+    x2 = width - 1
+    y2 = int(m * x2 + b)
+    return x1, y1, x2, y2
+
+def mainBlue(edgepoligonBlue, width):
+    m_orthogonal, b_orthogonal = find_orthogonal_line(x1, y1, x2, y2, egdepoligonBlue)
+    mx1 = 0
+    my1 = int(m_orthogonal * mx1 + b_orthogonal)
+    mx2 = width - 1
+    my2 = int(m_orthogonal * mx2 + b_orthogonal)
+    return mx1, my1, mx2, my2
+
+
+
+def getSideOfLine(x1, y1, x2, y2, x, y):
+    # Calculate the distance from the point to the line
+    distance = (y - y1) * (x2 - x1) - (x - x1) * (y2 - y1)
+    # The point is on the left side if the distance is positive
+    if distance > 0:
+        return "right"
+    # The point is on the right side if the distance is negative
+    elif distance < 0:
+        return "left"
+    # The point is on the line if the distance is zero
+    else:
+        return "on the line"
+
+def fit_line_through_points(edgePoligons):
+    x_coordinates = []
+    y_coordinates = []
+
+    for edgePoligon in edgePoligons:
+        x_coordinates.append(edgePoligon.x)
+        y_coordinates.append(edgePoligon.y)
+    # Fit a line (y = mx + b) through the points using linear regression
+    print(x_coordinates)
+    print(y_coordinates)
+    coefficients = np.polyfit(x_coordinates, y_coordinates, 1)
+
+    # Get the slope (m) and y-intercept (b)
+    m, b = coefficients
+    return m, b
+
+def find_orthogonal_line(x1, y1, x2, y2, edgePoligons):
+    blue_edgePoligons = list(dict.fromkeys(edgePoligons))
+    # Find the center of the blue edge poligons
+    x_coordinates = []
+    y_coordinates = []
+    for edgePoligon in blue_edgePoligons:
+        if getSideOfLine(x1, y1, x2, y2, edgePoligon.x, edgePoligon.y) == "left":
+            continue
+        x_coordinates.append(edgePoligon.x)
+        y_coordinates.append(edgePoligon.y)
+    x_center = sum(x_coordinates) / len(x_coordinates)
+    y_center = sum(y_coordinates) / len(y_coordinates)
+
+    # Find the slope of the line through the center of the blue edge poligons
+    m, b = fit_line_through_points(blue_edgePoligons)
+
+    # Find the slope of the orthogonal line
+    m_orthogonal = -1 / m
+
+    # Find the y-intercept of the orthogonal line
+    b_orthogonal = y_center - m_orthogonal * x_center
+
+    return m_orthogonal, b_orthogonal
+
